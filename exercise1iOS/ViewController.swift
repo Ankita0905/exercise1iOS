1e3dc607acde6ce0fdcc9d1393f02280c817931f
Ankita Jain <ankita.jain12342@gmail.com>
2020-01-09 15:08:32 -0500

    moved from top to down

--- exercise1iOS/ViewController.swift
+++ exercise1iOS/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        UIView.animate(withDuration: 5, animations: {
+            self.imageView.frame.origin.y+=750
+        }, completion: nil)
+//        UIView.animate(withDuration: 5, animations: {
+//            self.imageView.frame.origin.x+=1000
+//        }, completion: nil)
+        
+    }
+//        imageView.center=CGRect(origin: imageView.frame.origin, size: imageView!.frame)
+//                      UIView.animate(withDuration: 2) {
+//                          self.imageView.center=CGPoint(x: self.imageView.center.x+1000, y: self.imageView.center.y)
+        
         // Do any additional setup after loading the view.
     }
 
 
-}
+
 
