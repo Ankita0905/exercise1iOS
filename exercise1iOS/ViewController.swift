6c7c5bb2515151cf42a347a5b57dae63bf716f81
Ankita Jain <ankita.jain12342@gmail.com>
2020-01-10 15:04:27 -0500

    moved clockwise

--- exercise1iOS/ViewController.swift
+++ exercise1iOS/ViewController.swift
@@ -15,7 +15,61 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       self.imageView.frame.origin.y = self.space.bounds.minY
+       moveClockwise()
+        
+    }
+    
+    func moveCounterClockwise()
+    {
+        self.imageView.frame.origin.y = self.space.bounds.minY
+                self.imageView.frame.origin.x = self.space.bounds.minX
+                
+                   
+        UIView.animate(withDuration: 1, animations: {
+                   
+                    if self.imageView.frame.origin.y == self.space.bounds.minY && self.imageView.frame.origin.x == self.space.bounds.minX
+                    {
+        //            print(self.space.bounds.minX)
+        //            print(self.space.bounds.minY)
+                    self.imageView.frame.origin.y += self.space.bounds.height - self.imageView.bounds.height
+                        
+                        
+                    }
+                  
+                
+                
+                },completion:{ (true) in
+                    UIView.animate(withDuration: 1, animations: {
+                        if self.imageView.frame.origin.y == self.space.bounds.height - self.imageView.bounds.height && self.imageView.frame.origin.x == self.space.bounds.minX
+                                  {
+                                                self.imageView.frame.origin.x += self.space.bounds.width - self.imageView.bounds.width
+                                            }
+                    },completion: {
+                        (true) in
+                        UIView.animate(withDuration: 1, animations: {
+                            if self.imageView.frame.origin.y == self.space.bounds.height - self.imageView.bounds.height && self.imageView.frame.origin.x == self.space.bounds.width - self.imageView.bounds.width
+                                      {
+                                        self.imageView.frame.origin.y -= self.space.bounds.height - self.imageView.bounds.height
+                                                }
+                        },completion:{
+                            (true) in
+                            UIView.animate(withDuration: 1, animations: {
+                                if self.imageView.frame.origin.y == self.space.bounds.minY && self.imageView.frame.origin.x == self.space.bounds.width - self.imageView.bounds.width
+                                          {
+                                                        self.imageView.frame.origin.x -= self.space.bounds.width - self.imageView.bounds.width
+                                                    }
+                            },completion: {(true) in
+                                self.moveCounterClockwise()
+                            })
+                        })
+                        
+                    })
+                })
+    }
+    
+    func moveClockwise()
+    {
+        self.imageView.frame.origin.y = self.space.bounds.minY
                 self.imageView.frame.origin.x = self.space.bounds.minX
                 
                    
@@ -55,21 +109,15 @@ class ViewController: UIViewController {
                                           {
                                                         self.imageView.frame.origin.x -= self.space.bounds.width - self.imageView.bounds.width
                                                     }
-                            },completion: nil)
+                            },completion: {(true) in
+                                self.moveClockwise()
+                            })
                         })
                         
                     })
                 })
-//        UIView.animate(withDuration: 5, animations: {
-//            self.imageView.frame.origin.x+=1000
-//        }, completion: nil)
-        
     }
-//        imageView.center=CGRect(origin: imageView.frame.origin, size: imageView!.frame)
-//                      UIView.animate(withDuration: 2) {
-//                          self.imageView.center=CGPoint(x: self.imageView.center.x+1000, y: self.imageView.center.y)
-        
-        // Do any additional setup after loading the view.
+
     }
 
 
