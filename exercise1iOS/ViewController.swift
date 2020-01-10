4334364aacd7efddc2691d70cc65db91a510106e
Ankita Jain <ankita.jain12342@gmail.com>
2020-01-10 14:42:41 -0500

    rotated

--- exercise1iOS/ViewController.swift
+++ exercise1iOS/ViewController.swift
@@ -11,11 +11,55 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var space: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        UIView.animate(withDuration: 5, animations: {
-            self.imageView.frame.origin.y+=750
-        }, completion: nil)
+       self.imageView.frame.origin.y = self.space.bounds.minY
+                self.imageView.frame.origin.x = self.space.bounds.minX
+                
+                   
+        UIView.animate(withDuration: 1, animations: {
+                   
+                    if self.imageView.frame.origin.y == self.space.bounds.minY && self.imageView.frame.origin.x == self.space.bounds.minX
+                    {
+        //            print(self.space.bounds.minX)
+        //            print(self.space.bounds.minY)
+                    self.imageView.frame.origin.y += self.space.bounds.height - self.imageView.bounds.height
+                        print(self.space.bounds.height - self.imageView.bounds.height)
+                        print(self.imageView.frame.origin.y)
+                        print("\n")
+                        print( self.imageView.frame.origin.x)
+                        
+                    }
+                  
+                
+                
+                },completion:{ (true) in
+                    UIView.animate(withDuration: 1, animations: {
+                        if self.imageView.frame.origin.y == self.space.bounds.height - self.imageView.bounds.height && self.imageView.frame.origin.x == self.space.bounds.minX
+                                  {
+                                                self.imageView.frame.origin.x += self.space.bounds.width - self.imageView.bounds.width
+                                            }
+                    },completion: {
+                        (true) in
+                        UIView.animate(withDuration: 1, animations: {
+                            if self.imageView.frame.origin.y == self.space.bounds.height - self.imageView.bounds.height && self.imageView.frame.origin.x == self.space.bounds.width - self.imageView.bounds.width
+                                      {
+                                        self.imageView.frame.origin.y -= self.space.bounds.height - self.imageView.bounds.height
+                                                }
+                        },completion:{
+                            (true) in
+                            UIView.animate(withDuration: 1, animations: {
+                                if self.imageView.frame.origin.y == self.space.bounds.minY && self.imageView.frame.origin.x == self.space.bounds.width - self.imageView.bounds.width
+                                          {
+                                                        self.imageView.frame.origin.x -= self.space.bounds.width - self.imageView.bounds.width
+                                                    }
+                            },completion: nil)
+                        })
+                        
+                    })
+                })
 //        UIView.animate(withDuration: 5, animations: {
 //            self.imageView.frame.origin.x+=1000
 //        }, completion: nil)
